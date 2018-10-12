7b16e356f2d4f8e99fde35e45fa19447abce240b
Justin Focus <jf@3yourmind.com>
2018-10-12 19:44:49 +0200

    fix(master): Fix relative import in __init__.py

--- django_deprecate_fields/__init__.py
+++ django_deprecate_fields/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .deprecate_fields import *  # noqa
+from .deprecate_field import *  # noqa
