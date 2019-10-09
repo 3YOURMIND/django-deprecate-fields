5e1d13b0b29ed6923f8c398771943492464a9943
David Jovanovic <dj@3yourmind.com>
2019-10-09 10:01:56 +0200

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requirements = [
 
 setup(
     name='django-deprecate-fields',
-    version='0.0.3',
+    version='0.0.4',
 
     description='This package allows deprecating model fields and allows '
                 'removing them in a backwards compatible manner.',
