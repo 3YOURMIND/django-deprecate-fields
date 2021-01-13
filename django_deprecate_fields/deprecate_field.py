2d144c70928325533f99fd73f487c5d42794c180
Tonye Jack <jtonye@ymail.com>
2021-01-13 14:05:49 -0500

    Update deprecate_field.py

--- django_deprecate_fields/deprecate_field.py
+++ django_deprecate_fields/deprecate_field.py
@@ -2,6 +2,8 @@ import logging
 import sys
 import warnings
 
+from django.utils.encoding import force_str
+
 logger = logging.getLogger(__file__)
 
 
@@ -11,28 +13,27 @@ class DeprecatedField(object):
     deprecate_field function instead.
     """
 
-    def __init__(self, val):
+    def __init__(self, val=None, use_current=False, field_name=None):
+        self.use_current = use_current
+        self.field_name = field_name
         self.val = val
 
-    def _get_name(self, obj):
-        """
-        Try to find this field's name in the model class
-        """
-        for k, v in type(obj).__dict__.items():
-            if v is self:
-                return k
-        return "<unknown>"
-
-    def __get__(self, obj, type=None):
+    def __get__(self, instance, type=None):
         msg = "accessing deprecated field %s.%s" % (
-            obj.__class__.__name__,
-            self._get_name(obj),
+            instance.__class__.__name__,
+            self.field_name,
         )
         warnings.warn(msg, DeprecationWarning, stacklevel=2)
         logger.warning(msg)
-        if not callable(self.val):
-            return self.val
-        return self.val()
+        
+        val = self.val
+        
+        if self.use_current and val is None:
+            val = getattr(instance, self.field_name, None)
+        
+        if not callable(val):
+            return val
+        return val()
 
     def __set__(self, obj, val):
         msg = "writing to deprecated field %s.%s" % (
@@ -44,7 +45,7 @@ class DeprecatedField(object):
         self.val = val
 
 
-def deprecate_field(field_instance, return_instead=None):
+def deprecate_field(field_instance, return_instead=None, use_current=False):
     """
     Can be used in models to delete a Field in a Backwards compatible manner.
     The process for deleting old model Fields is:
@@ -58,6 +59,9 @@ def deprecate_field(field_instance, return_instead=None):
     the field will pretend to have
     """
     if not set(sys.argv) & {"makemigrations", "migrate", "showmigrations"}:
+        if return_instead is None and use_current:
+            field_name = force_str(field_instance.name)
+            return DeprecatedField(use_current=use_current, field_name=field_name)
         return DeprecatedField(return_instead)
 
     field_instance.null = True
